897eda0fb4ae3de87ff9246fe315a49451641d8c
ligeronahill <provotorov@inbox.ru>
2023-11-07 20:49:49 +0300

    tutor-db pg connect

---
+++ tutor-db/src/database.sql
@@ -0,0 +1,19 @@
+/* Drop table if it already exists*/
+DROP TABLE IF EXISTS ezy_course_c4;
+/* Create a table. */
+/* Note: Don't put a comma after last field */
+CREATE TABLE ezy_course_c4
+(
+course_id SERIAL PRIMARY KEY,
+tutor_id INT NOT NULL,
+course_name VARCHAR(140) NOT NULL,
+posted_time TIMESTAMP DEFAULT now()
+);
+/* Load seed data for testing */
+INSERT INTO ezy_course_c4
+(course_id,tutor_id, course_name,posted_time)
+VALUES(1, 1, 'First course', '2020-12-17 05:40:00');
+
+INSERT INTO ezy_course_c4
+(course_id, tutor_id, course_name,posted_time)
+VALUES(2, 1, 'Second course', '2020-12-18 05:45:00');
